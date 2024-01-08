59b37c10c67d01be3ff352d305b1dc249101bfeb
Anjali261 <mail2anjali.singh2610@gmail.com>
2024-01-08 12:46:16 +0530

    Databse Connectivity

--- server/index.js
+++ server/index.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const app = express();
-
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+dotenv.config();
 const PORT = 4000
+
+
+// Database Connectivity
+mongoose.set('strictQuery', false);
+const connectDB = async() =>{
+    try{
+        const con = await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser:true,
+
+        })
+        console.log(`MongoDB connected: ${con.connection.host}`);
+        
+    }catch(error){ 
+        console.log(error);
+        process.exit(1);
+
+    }
+}
+connectDB(); 
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
     });
